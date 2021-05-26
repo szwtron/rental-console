f9ac3bd6ccda6837548c67876075325d65d653fa
Juliushoho <juliuscannavaro11@gmail.com>
2021-05-26 14:00:43 +0700

    Customer Registration

--- application/views/customer/dashboard.php
+++ application/views/customer/dashboard.php
@@ -1,6 +1,6 @@
     <body>
         <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container px-4 px-lg-5">
                 <a class="navbar-brand" href="#!">Start Bootstrap</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
@@ -27,13 +27,13 @@
                     </form>
                 </div>
             </div>
-        </nav>
+        </nav> -->
         <!-- Header-->
         <header class="bg-dark py-5">
             <div class="container px-4 px-lg-5 my-5">
                 <div class="text-center text-white">
                     <h1 class="display-4 fw-bolder">Shop in style</h1>
-                    <p class="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
+                    <p class="lead fw-normal text-white-50 mb-0">With this shop homepage template</p>
                 </div>
             </div>
         </header>
---
+++ application/views/register_form.php
@@ -0,0 +1,84 @@
+<body>
+  <div id="app">
+    <section class="section">
+      <div class="container mt-5">
+        <div class="row">
+          <div class="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-8 offset-lg-2 col-xl-8 offset-xl-2">
+            <div class="login-brand">
+              <img src="<?php echo base_url('assets/assets_stisla/')?>/assets/img/stisla-fill.svg" alt="logo" width="100" class="shadow-light rounded-circle">
+            </div>
+
+            <div class="card card-primary">
+              <div class="card-header"><h4>Register</h4></div>
+
+              <div class="card-body">
+                    <form method="POST" action="<?php echo base_url('register') ?>">
+                        <div class="row">
+                            <div class="form-group col-6">
+                                <label for="nama">Nama</label>
+                                <input type="text" class="form-control" name="nama" autofocus>
+                                <?php echo form_error('nama', '<div class="text-small text-danger">', '</div>')?>
+                            </div>
+
+                            <div class="form-group col-6">
+                                <label for="username">Username</label>
+                                <input type="text" class="form-control" name="username">
+                                <?php echo form_error('username', '<div class="text-small text-danger">', '</div>')?>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="alamat">Alamat</label>
+                            <input type="text" class="form-control" name="alamat">
+                            <?php echo form_error('alamat', '<div class="text-small text-danger">', '</div>')?>
+                        </div>
+                        
+                        <div class="row">
+                            <div class="form-group col-6">
+                                <label for="gender" class="d-block">Gender</label>
+                                    <select class="form-control" name="gender">
+                                        <option value="">--Pilih Gender--</option>
+                                        <option>Laki-laki</option>
+                                        <option>Perempuan</option>
+                                    </select>
+                                <?php echo form_error('gender', '<div class="text-small text-danger">', '</div>')?>
+                            </div>
+
+                            <div class="form-group col-6">
+                                <label for="no_telepon">No. Telepon</label>
+                                <input type="text" class="form-control" name="no_telepon">
+                                <?php echo form_error('no_telepon', '<div class="text-small text-danger">', '</div>')?>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="form-group col-6">
+                                <label for="email">Email</label>
+                                <input type="email" class="form-control" name="email">
+                                <div class="invalid-feedback"></div>
+                                <?php echo form_error('email', '<div class="text-small text-danger">', '</div>')?>
+                            </div>
+
+                            <div class="form-group col-6">
+                                <label for="password" class="d-block">Password</label>
+                                <input type="password" class="form-control" name="password">
+                                <?php echo form_error('password', '<div class="text-small text-danger">', '</div>')?>
+                            </div>
+                        </div> 
+
+                        <div class="form-group">
+                        <button type="submit" class="btn btn-primary btn-lg btn-block">
+                        Register
+                        </button>
+                        
+                    </form>
+              </div>
+            </div>
+            <div class="simple-footer">
+              Copyright &copy; Stisla 2018
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
