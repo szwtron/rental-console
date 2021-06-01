4546c5174e9346b33f99e860306ae897d588f203
Stainley <szwtron@gmail.com>
2021-06-01 20:50:20 +0700

    fix admin dashboard appearance

--- assets/assets_stisla/assets/js/page/index-0.js
+++ assets/assets_stisla/assets/js/page/index-0.js
@@ -1,44 +1,55 @@
 "use strict";
 
 var statistics_chart = document.getElementById("myChart").getContext('2d');
+var data_tanggal = [];
+var data_revenue = [];
 
-var myChart = new Chart(statistics_chart, {
-  type: 'line',
-  data: {
-    labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-    datasets: [{
-      label: 'Statistics',
-      data: [640, 387, 530, 302, 430, 270, 488],
-      borderWidth: 5,
-      borderColor: '#6777ef',
-      backgroundColor: 'transparent',
-      pointBackgroundColor: '#fff',
-      pointBorderColor: '#6777ef',
-      pointRadius: 4
-    }]
-  },
-  options: {
-    legend: {
-      display: false
-    },
-    scales: {
-      yAxes: [{
-        gridLines: {
-          display: false,
-          drawBorder: false,
+$.post("http://localhost/rental-console/admin/dashboard/getData", 
+  function(data) {
+    var obj = JSON.parse(data);
+    $.each(obj, function(test, item){
+      data_tanggal.push(item.fromDate);
+      data_revenue.push(item.Revenue);
+    });
+
+    var myChart = new Chart(statistics_chart, {
+      type: 'line',
+      data: {
+        labels: data_tanggal,
+        datasets: [{
+          label: 'Gross Revenue',
+          data: data_revenue,
+          borderWidth: 5,
+          borderColor: '#6777ef',
+          backgroundColor: 'transparent',
+          pointBackgroundColor: '#fff',
+          pointBorderColor: '#6777ef',
+          pointRadius: 4
+        }]
+      },
+      options: {
+        legend: {
+          display: false
         },
-        ticks: {
-          stepSize: 150
-        }
-      }],
-      xAxes: [{
-        gridLines: {
-          color: '#fbfbfb',
-          lineWidth: 2
-        }
-      }]
-    },
-  }
+        scales: {
+          yAxes: [{
+            gridLines: {
+              display: false,
+              drawBorder: false,
+            },
+            ticks: {
+              stepSize: 150
+            }
+          }],
+          xAxes: [{
+            gridLines: {
+              color: '#fbfbfb',
+              lineWidth: 2
+            }
+          }]
+        },
+      }
+    });
 });
 
 $('#visitorMap').vectorMap(
