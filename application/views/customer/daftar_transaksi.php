5068ecc5c8ebb70d59eec3fdd5e019c7c90aea67
Christian Halim <chris.richard.halim@gmail.com>
2021-06-01 20:39:53 +0700

    test

--- application/views/customer/daftar_transaksi.php
+++ application/views/customer/daftar_transaksi.php
@@ -4,6 +4,8 @@
             <h1>Daftar Transaksi</h1>
         </div>
 
+        
+
         <table class="table table-bordered table-striped">
             <tr>
                 <th>No</th>
@@ -54,14 +56,14 @@
                     }?></td>
 
                     <td>
-                    <?php if($tr->status == "Sedang Dikirim"){
-                        echo "<span class='badge alert-warning'>$tr->status</span>";
-                    }else if($tr->status == "Sudah Dikirim"){
-                        echo "<span class='badge alert-success'>$tr->status</span>";
-                    }else if($tr->status == "Siap di Pick-up"){
-                        echo "<span class='badge alert-warning'>$tr->status</span>";
-                    }else if($tr->status == "Selesai"){
-                        echo "<span class='badge alert-success'>$tr->status</span>";
+                    <?php if($tr->status_invoice == "Sedang Dikirim"){
+                        echo "<span class='badge alert-warning'>$tr->status_invoice</span>";
+                    }else if($tr->status_invoice == "Sudah Dikirim"){
+                        echo "<span class='badge alert-success'>$tr->status_invoice</span>";
+                    }else if($tr->status_invoice == "Siap di Pick-up"){
+                        echo "<span class='badge alert-warning'>$tr->status_invoice</span>";
+                    }else if($tr->status_invoice == "Selesai"){
+                        echo "<span class='badge alert-success'>$tr->status_invoice</span>";
                     }?>
                     </td>
 
@@ -73,19 +75,20 @@
 
                     <!-- Status -->
                     <td>
+                        <a href="<?php echo base_url('customer/invoice_customer/detail_invoice/').$tr->id_invoice?>" ><button class="btn btn-sm btn-primary m-2">Details</button></a>
                         <?php 
                         
-                            if($tr->status == "Siap di Pick-up"){
+                            if($tr->status_invoice == "Sudah Dikirim"){
                                 ?>
                                     <div class="row">
-                                        <a href="<?php echo base_url('admin/transaction/transaction_selesai')?>" ><button class="btn btn-sm btn-success m-2"><i class="bi bi-check"></i></button></a>
+                                        <a href="<?php echo base_url('customer/invoice_customer/transaction_diterima/').$tr->id_invoice?>" ><button class="btn btn-sm btn-success m-2"><i class="bi bi-check"></i></button></a>
                                         
                                     </div>
                                 <?php
                             }
                         
                         ?>
-                        <a href="<?php echo base_url('admin/transaction/transaction_batal')?>" ><button class="btn btn-sm btn-danger m-2"><i class="bi bi-x"></i></button></a>
+                        <a href="<?php echo base_url('customer/invoice_customer/transaction_batal/').$tr->id_invoice?>" ><button class="btn btn-sm btn-danger m-2"><i class="bi bi-x"></i></button></a>
                     </td>
                     
                 </tr>
