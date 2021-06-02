acb42e0be87e6ac9efb87e401646770eb364b094
Christian Halim <chris.richard.halim@gmail.com>
2021-06-02 15:24:49 +0700

    Fix Tampilan All Console

--- application/views/customer/dashboard.php
+++ application/views/customer/dashboard.php
@@ -357,7 +357,7 @@
         <section class="bg-white">
         <!-- <?php echo $this->session->flashdata('pesan') ?> -->
         
-            <div class="bg-white container px-4 px-lg-5 mt-5">
+            <div class="bg-white px-4 px-lg-5 mt-5">
                 <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
                 <?php foreach($console as $cs) : ?> 
                     <div class="col mb-5">
