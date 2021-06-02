80d9fc5d4584b4d0abd734d499f81ddd792f7845
Christian Halim <chris.richard.halim@gmail.com>
2021-06-02 19:17:13 +0700

    Dashboard Update

--- application/views/customer/dashboard.php
+++ application/views/customer/dashboard.php
@@ -21,13 +21,16 @@
         <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                <img class="d-block w-100" src="<?php echo base_url('assets/assets_shop/img/ps5-games-background.jpg') ?>" alt="First slide">
+                <img class="d-block w-100" style="height:563.016px;!important" src="<?php echo base_url('assets/assets_shop/img/ps5-games-background.jpg') ?>" alt="First slide">
                 </div>
                 <div class="carousel-item">
-                <img class="d-block w-100" src="<?php echo base_url('assets/assets_shop/img/ps5-games-background.jpg') ?>" alt="Second slide">
+                <img class="d-block w-100"  src="<?php echo base_url('assets/assets_shop/img/explore-switch.webp') ?>" alt="Third slide">
                 </div>
                 <div class="carousel-item">
-                <img class="d-block w-100" src="<?php echo base_url('assets/assets_shop/img/ps5-games-background.jpg') ?>" alt="Third slide">
+                <img class="d-block w-100" style="height:563.016px;!important" src="<?php echo base_url('assets/assets_shop/img/ps5-entertainment-banner.png') ?>" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                <img class="d-block w-100" style="height:563.016px;!important" src="<?php echo base_url('assets/assets_shop/img/nintendo-hero.jpg') ?>" alt="Third slide">
                 </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
