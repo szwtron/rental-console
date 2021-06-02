b2173bfcaf075da709eccd74a053b55f87bd2f06
Stainley <szwtron@gmail.com>
2021-06-02 14:39:49 +0700

    Login and register update logo

--- application/views/form_login.php
+++ application/views/form_login.php
@@ -5,7 +5,7 @@
         <div class="row">
           <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
             <div class="login-brand">
-              <img src="<?php echo base_url('assets/assets_stisla/')?>/assets/img/stisla-fill.svg" alt="logo" width="100" class="shadow-light rounded-circle">
+              <img src="<?php echo base_url('assets/')?>icon-removebg-preview-svg.svg" alt="logo" width="100" class="shadow-light rounded-circle">
             </div>
 
             <div class="card card-primary">
@@ -48,7 +48,7 @@
               Don't have an account? <a href="<?php echo base_url('register')?>">Create One</a>
             </div>
             <div class="simple-footer">
-              Copyright &copy; Stisla 2018
+              Copyright &copy; 2021
             </div>
           </div>
         </div>
